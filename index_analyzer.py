392f3b05bd749215d761402e8ffe1669c8d9b8d1
supermt <supermt@supermtdeMacBook-Air.local>
2019-09-25 14:09:59 +0900

    painting figure

--- index_analyzer.py
+++ index_analyzer.py
@@ -3,7 +3,9 @@
 # import re
 # import json
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+
 
 INDEX_NAME = "index_file.csv"
 
@@ -15,9 +17,10 @@ xs = []
 ys = []
 
 y_c = 0
+d={}
 while line:
     kv = line.split(",")
-    x = long(kv[0].ljust(24, '0'))
+    x = long(kv[0].ljust(48, '0'))
     y = long(kv[1].replace("\n",""),16)
     
     # if y_c > y:
@@ -28,11 +31,40 @@ while line:
     ys.append(y)
     line = f.readline()
     y_c = y
-fig = plt.figure()
 
-axes = fig.subplots(1)
-ys.sort()
+    d[x]=y
 
-axes.plot(ys)
+d = sorted(d.items(), key = lambda kv:(kv[1], kv[0]))
+xs=[]
+ys=[]
+for entry in d:
+    xs.append(entry[0])
+    ys.append(entry[1])
+
+
+fig, axs = plt.subplots(2, 5)
+
+# 0~4 first row
+# 5~9
+
+for row in range(2):
+    start = 500 + row * 100 
+    for column in range(5):
+        window = 20 * (2 ** column)
+        keyrange = ys[start:start+window]
+
+        axs[row][column].set_title("start point: " + str(start) + 
+        "\nomwindow size " + str(window) + 
+        "\n space range " + '%.2E' % (max(keyrange) - min(keyrange)),
+        fontsize=10)
+        axs[row][column].scatter(xs[start:start+window],ys[start:start+window])
+
+# window = 40
+# start = 600
+
+# fig.suptitle('window size = ' + str(window), fontsize=16)
+
+# y_x_pair = OrderedDict(sorted(d.items(), key=lambda x: x[1]))
+# plt.scatter(xs[start:start+window],ys[start:start+window])
 
 plt.show()
